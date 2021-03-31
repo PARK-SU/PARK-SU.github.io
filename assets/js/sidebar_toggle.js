abe2b6e7392dd655dbc72ae72b0f243314cc999f
PARK-SU <50171671+PARK-SU@users.noreply.github.com>
2021-04-01 01:41:38 +0900

    April Fool's Day
    만우절 기념 2019년 4월에 쓰던 디자인으로 복귀.

--- assets/js/sidebar_toggle.js
+++
@@ -1,5 +0,0 @@
-// Sidebar toggled
-if (window.innerWidth <= 768) {
-	document.querySelector('body').classList.toggle('sidebar-toggled');
-	document.querySelector('.sidebar').classList.toggle('toggled')
-}
--- assets/js/table.js
+++ assets/js/table.js
@@ -1,195 +1,199 @@
-// Difficulty Table
-$(document).ready(function() {
-    $.getJSON($("meta[name=bmstable]").attr("content"), function(header) {
-        mark = header.symbol;
-        getLanguage = navigator.language;
-        if (header.last_update != null) $("#update").text("Last Update : " + header.last_update);
-        if (header.enum_level_order != null) $.fn.dataTable.enum(header.enum_level_order);
-        if (header.changelog_url != null) makeChangelog(header.changelog_url);
-        makeBMSTable(header);
-    });
-});
-
-// Changelog
-function makeChangelog(url) {
-    var $changelog = $("#changelog");
-    var $show_log = $("#show_log");
-    $changelog.load(url);
-    $show_log.click(function() {
-        if ($changelog.css("display") == "none" && $show_log.html() == "View Changelog") {
-            $changelog.show();
-            $show_log.html("Hide Changelog");
-        } else {
-            $changelog.hide();
-            $show_log.html("View Changelog");
-        }
-    });
+// メニュー開閉
+function menu(tName) {
+    var tMenu = document.getElementById(tName).style;
+    if (tMenu.display == "none") {
+        tMenu.display = "block";
+    } else {
+        tMenu.display = "none";
+    }
 }
 
-// BMS table
-function makeBMSTable(header) {
-    $("#table_diff").DataTable({
-        paging: false,
-        info: false,
-        lengthChange: false,
-
-        language: {
-            url: getLanguage === "ko-KR" || getLanguage === "ko-kr" || getLanguage === "ko"
-                 ? "//cdn.datatables.net/plug-ins/1.10.24/i18n/Korean.json"
-                 : "//cdn.datatables.net/plug-ins/1.10.24/i18n/Japanese.json"
-        },
-
-        ajax: {
-            "url": typeof data_link != "undefined"
-                   ? data_link
-                   : header.data_url,
-            "dataSrc": ""
-        },
-
-        columns: typeof tableColumns != "undefined"
-                 ? tableColumns
-                 : defaultColumns,
-
-        createdRow: function(row, data) {
-            if (data.state == 1) {
-                $(row).addClass("table-primary"); // New
-            }
-            if (data.state == 2) {
-                $(row).addClass("table-warning"); // Fixed
-            }
-            if (data.state == 3) {
-                $(row).addClass("table-success"); // Chartin
-            }
-            if (data.state == 4) {
-                $(row).addClass("table-secondary"); // Darkhistory
-            }
-            if (data.state == 5) {
-                $(row).addClass("table-info"); // Recommend
-            }
-        },
-
-        initComplete: function() { // Filter
-            if (getLanguage === "ko-KR" || getLanguage === "ko-kr" || getLanguage === "ko") { // Filter by Korean
-                this.api().columns(0).every(function() {
-                    var column = this;
-                    var select = $("<div class='dataTables_length' style='float:left'>레벨 필터: <select class='form-control'><option value=''>Level</option></select></div>")
-                        .prependTo($("#table_diff_wrapper"))
-                        .on("change", function() {
-                            var val = $.fn.dataTable.util.escapeRegex(
-                                $(this).find("select").val()
-                            );
-                            column.search(val ? "^" + val + "$" : "", true, false).draw();
-                        });
-                    column
-                        .data()
-                        .unique()
-                        .sort(function(a, b) {
-                            return parseInt(a) - parseInt(b); // a - b는 오름차순, b - a 는 내림차순
-                        })
-                        .each(function(d, j) {
-                            select.find("select").append("<option value='" + mark + d + "'>" + d + "</option>")
-                        });
-                });
-            } else { // Filter by Japanese
-                this.api().columns(0).every(function() {
-                    var column = this;
-                    var select = $("<div class='dataTables_length' style='float:left'>レベルでフィルタ: <select class='form-control'><option value=''>Level</option></select></div>")
-                        .prependTo($("#table_diff_wrapper"))
-                        .on("change", function() {
-                            var val = $.fn.dataTable.util.escapeRegex(
-                                $(this).find("select").val()
-                            );
-                            column.search(val ? "^" + val + "$" : "", true, false).draw();
-                        });
-                    column
-                        .data()
-                        .unique()
-                        .sort(function(a, b) {
-                            return parseInt(a) - parseInt(b); // a - b는 오름차순, b - a 는 내림차순
-                        })
-                        .each(function(d, j) {
-                            select.find("select").append("<option value='" + mark + d + "'>" + d + "</option>")
-                        });
-                });
-            }
-        }
-    });
+function getJSON(url, callback) {
+    var req = new XMLHttpRequest();
+    req.onload = function() {
+        var data = JSON.parse(req.responseText);
+        return callback(data);
+    };
+    req.open("GET", url, true);
+    req.send(null);
 }
 
-// Table Data
-var tableData = {
-
-    // Level (IR)
-    tableLevel: function(data, type, row) {
-        return "<a href='http://www.dream-pro.info/~lavalse/LR2IR/search.cgi?mode=ranking&bmsmd5=" + row.md5 + "' target='_blank'>" + mark + data + "</a>";
-    },
-
-    // Title (Movie Link)
-    tableTitle: function(data, type, row) {
-        if (row.movie_link != "") {
-            if (data != "") {
-                return "<a href='" + row.movie_link + "' target='_blank'>" + data + "</a>";
-            } else {
-                return "<a href='" + row.movie_link + "' target='_blank'>" + row.movie_link + "</a>";
-            }
-        } else {
-            if (data != "") {
-                return data;
-            } else {
-                return "";
-            }
+getJSON(document.querySelector("meta[name=bmstable]").getAttribute("content"),
+    function(header) {
+        if (header.last_update != null) {
+            var last_update = new Date(header.last_update);
+            document.getElementById("update").textContent = last_update.toLocaleDateString();
         }
-    },
+        getJSON(header.data_url,
+            function(information) {
+                if (header.level_order) {
+                    makeBMSTable(information, header.symbol, header.level_order);
+                } else {
+                    makeBMSTable(information, header.symbol);
+                }
+            });
+    });
 
-    // Artist (Song DL)
-    tableArtist: function(data, type, row) {
+
+// ソートのための引数追加
+function makeBMSTable(info, mark, order) {
+    // orderが未指定の場合はnull
+    if (typeof order === "undefined") {
+        order = null;
+    }
+
+    var x = "";
+    var ev = "";
+    var total = 0;
+    var count = 0;
+    var obj = document.querySelector("#table_int");
+
+    // ソート
+    if (order != "" && order != null) {
+        // header.jsonにsortが存在する場合は指定順->タイトル順にソート
+
+        var orderAry = [];
+        for (var l = 0; l < order.length; l++) {
+            orderAry.push(order[l].toString());
+        }
+
+        for (var j = 0; j < info.length; j++) {
+            var index = orderAry.indexOf(info[j]["level"]);
+            info[j]["_index"] = index;
+        }
+
+        info.sort(function(a, b) {
+            if (a["_index"] < b["_index"]) {
+                return -1;
+            } else if (a["_index"] > b["_index"]) {
+                return 1;
+            } else if (a["title"].toLowerCase() < b["title"].toLowerCase()) {
+                return -1;
+            } else if (a["title"].toLowerCase() > b["title"].toLowerCase()) {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+        for (var k = 0; k < info.length; k++) {
+            delete info[k]["_index"];
+        }
+    } else {
+        // そうでない場合はレベル順->タイトル順にソート
+        info.sort(
+            function(a, b) {
+                var aLv = a["level"].toString();
+                var bLv = b["level"].toString();
+                if (isNaN(a["level"]) == false && isNaN(b["level"]) == false) {
+                    return a["level"] - b["level"];
+                } else if (aLv < bLv) {
+                    return -1;
+                } else if (aLv > bLv) {
+                    return 1;
+                } else if (a["title"].toLowerCase() < b["title"].toLowerCase()) {
+                    return -1;
+                } else if (a["title"].toLowerCase() > b["title"].toLowerCase()) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        );
+    }
+
+    // 表のクリア
+    obj.innerHTML = "<thead><tr><td>Level</td><td>Movie</td><td>Chart</td><td>Title</td><td>Artist</td><td>DL</td><td>Date</td><td>Comment</td></tr></thead>"
+    var obj_sep = null;
+    for (var i = 0; i < info.length; i++) {
+        // 難度ごとの区切り
+        if (x != info[i].level) {
+            // 前の区切りに譜面数、平均密度を追加
+            if (obj_sep != null) {
+                obj_sep.innerHTML = "<td colspan='8'>" + "<b>" + mark + x + " (" + count + " Charts)</b></td>";
+            }
+            obj_sep = document.createElement("tr");
+            obj_sep.setAttribute("class", "tr_separate");
+            obj_sep.setAttribute("id", mark + info[i].level);
+            obj.appendChild(obj_sep);
+            total += count;
+            count = 0;
+            x = info[i].level;
+        }
+        // 本文
+        var str = "";
+        var ele = document.createElement("tr");
+        if (info[i].state == 1) {
+            ele.setAttribute("class", "state1");
+        } else if (info[i].state == 2) {
+            ele.setAttribute("class", "state2");
+        } else if (info[i].state == 3) {
+            ele.setAttribute("class", "state3");
+        } else if (info[i].state == 4) {
+            ele.setAttribute("class", "state4");
+        } else if (info[i].state == 5) {
+            ele.setAttribute("class", "state5");
+        } else if (info[i].state == 6) {
+            ele.setAttribute("class", "state6");
+        } else {
+            ele.setAttribute("class", "tr_normal");
+        }
+
+        // レベル表記
+        str += "<td width='1%'>" + mark + x + "</td>";
+        // 動画
+        if (info[i].movie_link != "" && info[i].movie_link != null) {
+            // ニコニコ
+            str += "<td width='1%'><a href='" + info[i].movie_link + "' target='_blank'>■</a></td>";
+        } else {
+            str += "<td width='1%'></td>";
+        }
+        // 譜面画像
+        str += "<td width='1%'><a href='http://www.ribbit.xyz/bms/score/view?p=1&md5=" + info[i].md5 + "' target='_blank'>■</a></td>";
+
+        // タイトル
+        str += "<td width='20%'>" + "<a href='http://www.dream-pro.info/~lavalse/LR2IR/search.cgi?mode=ranking&bmsmd5=" + info[i].md5 + "' target='_blank'>" + info[i].title + "</a></td>";
+        // アーティスト
         var astr = "";
-        if (row.url != "") {
-            if (data != "") {
-                astr = "<a href='" + row.url + "' target='_blank'>" + data + "</a>";
+        if (info[i].url != "" && info[i].url != null) {
+            if (info[i].artist != "" && info[i].artist != null) {
+                astr = "<a href='" + info[i].url + "'>" + info[i].artist + "</a>";
             } else {
-                astr = "<a href='" + row.url + "' target='_blank'>" + row.url + "</a>";
+                astr = "<a href='" + info[i].url + "'>" + info[i].url + "</a>";
             }
         } else {
-            if (data != "") {
-                astr = data;
+            if (info[i].artist != "" && info[i].artist != null) {
+                astr = info[i].artist;
             }
         }
-        if (row.url_pack != "") {
-            if (row.name_pack != "") {
-                astr += "<br>(<a href='" + row.url_pack + "' target='_blank'>" + row.name_pack + "</a>)";
+        if (info[i].url_pack != "" && info[i].url_pack != null) {
+            if (info[i].name_pack != "" && info[i].name_pack != null) {
+                astr += "<br>(<a href='" + info[i].url_pack + "'>" + info[i].name_pack + "</a>)";
             } else {
-                astr += "<br>(<a href='" + row.url_pack + "' target='_blank'>" + row.url_pack + "</a>)";
+                astr += "<br>(<a href='" + info[i].url_pack + "'>" + info[i].url_pack + "</a>)";
             }
         } else {
-            if (row.name_pack != "") {
-                astr += "<br>(" + row.name_pack + ")";
+            if (info[i].name_pack != "" && info[i].name_pack != null) {
+                astr += "<br>(" + info[i].name_pack + ")";
             }
         }
-        return astr;
-    },
-
-    // Pattern Download
-    tablePattern: function(data, type, row) {
-        if (row.url_diff != "") {
-            if (data != "") {
-                return "<a href='" + row.url_diff + "' target='_blank'>" + data + "</a>";
+        str += "<td width='20%'>" + astr + "</td>";
+        // 差分
+        if (info[i].url_diff != "" && info[i].url_diff != null) {
+            if (info[i].name_diff != "" && info[i].name_diff != null) {
+                str += "<td width='3%'><a href='" + info[i].url_diff + "'>" + info[i].name_diff + "</a></td>";
             } else {
-                return "<a href='" + row.url_diff + "'><i class='far fa-lg fa-arrow-alt-circle-down'></i></a>";
+                str += "<td width='3%'><a href='" + info[i].url_diff + "'>DL</a></td>";
             }
         } else {
-            if (data != "") {
-                return data;
+            if (info[i].name_diff != "" && info[i].name_diff != null) {
+                str += "<td width='3%'>" + info[i].name_diff + "</td>";
             } else {
-                return "同梱";
+                str += "<td width='3%'>同梱</td>";
             }
         }
-    },
-
-    // Added Date
-    tableDate: function(data, type, row) {
-        if (data != "") {
-            var addDate = new Date(data);
+        // Added Date
+        if (info[i].date != "" && info[i].date != null) {
+            var addDate = new Date(info[i].date);
             var year = addDate.getFullYear();
             var month = addDate.getMonth() + 1;
             var day = addDate.getDate();
@@ -199,73 +203,26 @@ var tableData = {
             if (day < 10) {
                 day = "0" + day;
             }
-            return year + "." + month + "." + day;
+            str += "<td width='3%'>" + year + "." + month + "." + day + "</td>";
         } else {
-            return "";
+            str += "<td width='3%'></td>";
         }
-    },
-
-    // View Pattern
-    tableScore: function(data, type, row) {
-        return "<a href='http://www.ribbit.xyz/bms/score/view?md5=" + data + "' target='_blank'><i class='fas fa-lg fa-music'></i></a>";
-    },
-
-    // Comment
-    tableComment: function(data, type, row) {
-        if (getLanguage === "ko-KR" || getLanguage === "ko-kr" || getLanguage === "ko") {
-            return row.comment_kr;
+        // コメント
+        if (navigator.language === "ko-KR" || navigator.language === "ko-kr" || navigator.language === "ko") {
+            str += "<td width='25%'>" + info[i].comment_kr + "</div></td>";
         } else {
-            return data;
+            str += "<td width='25%'>" + info[i].comment + "</div></td>";
         }
-    },
-};
+        ele.innerHTML = str;
+        obj.appendChild(ele);
+        count++;
+    }
 
-var defaultColumns = [
-    {
-        "title": "Level<br>(IR)",
-        "width": "1%",
-        "data": "level",
-        "type": "natural-nohtml",
-        "render": tableData.tableLevel
-    },
-    {
-        "title": "Title<br>(Movie)",
-        "width": "30%",
-        "data": "title",
-        "render": tableData.tableTitle
-    },
-    {
-        "title": "Artist<br>(Song DL)",
-        "width": "30%",
-        "data": "artist",
-        "render": tableData.tableArtist
-    },
-    {
-        "title": "DL",
-        "width": "1%",
-        "data": "name_diff",
-        "orderable": false,
-        "searchable": false,
-        "render": tableData.tablePattern
-    },
-    {
-        "title": "Date",
-        "width": "5%",
-        "data": "date",
-        "render": tableData.tableDate
-    },
-    {
-        "title": "<i class='fas fa-lg fa-search'></i>",
-        "width": "1%",
-        "data": "md5",
-        "orderable": false,
-        "searchable": false,
-        "render": tableData.tableScore
-    },
-    {
-        "title": "Comment",
-        "width": "30%",
-        "data": "comment",
-        "render": tableData.tableComment
-    },
-];
+
+    // 最後の区切り処理
+    // マークが抜け落ちてたので追加
+    if (obj_sep != null) {
+        obj_sep.innerHTML = "<td colspan='8'>" + "<b>" + mark + x + " (" + count + " Charts)</b></td>";
+    }
+    document.querySelector("#total").textContent = (total + count);
+}
